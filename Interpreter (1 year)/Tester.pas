d6d0de77c026791fb7eea3fe11d024840c466fd8
Almeswe <52577119+Almeswe@users.noreply.github.com>
2021-04-16 01:33:54 +0300

    tester demo added

--- Interpreter (1 year)/Tester.pas
+++ Interpreter (1 year)/Tester.pas
@@ -16,11 +16,14 @@ type TTester = record
   procedure reset();
 
   procedure test_for_errors();
-  procedure test_expr();
   procedure test_operand();
+  procedure test_operator();
 
   procedure get_next_token();
-  procedure append_error(msg : string);
+  procedure append_error(msg : string; pos : TPosition);
+  procedure append_error_formatted(expected : string; met : string);
+
+  function match(kind : TTokenKind; offset : int = 0) : bool;
 end;
 
 implementation
@@ -44,69 +47,136 @@ begin
 end;
 
 procedure TTester.test_for_errors();
+var i : int;
+var paren_pairs : int;
 begin
   if not self.inited then
     raise Exception.Create('Call init procedure first!');
-  self.get_next_token;
-  test_expr();
-end;
+  paren_pairs := 0;
+  get_next_token;
+  //
+  for i := 0 to length(self.tokens)-1 do begin
+  //
+    case self.curr_token.kind of
+      TTokenKind.Constant, TTokenKind.Variable: begin
+        self.test_operand;
+      end;
 
-procedure TTester.test_expr();
-begin
-  if not self.inited then
-    raise Exception.Create('Call init procedure first!');
+      TTokenKind.Op: begin
+        self.test_operator;
+      end;
 
-  self.test_operand();
-  if self.curr_token.kind = TTokenKind.Op then begin
-      self.get_next_token;
-      self.test_expr;
-  end
-  else
-    if self.curr_token_num+1 > length(self.tokens) then
-      self.append_error('Operator expected, but met: ' + self.curr_token.data);
+      TTokenKind.OpenParen: begin
+        inc(paren_pairs);
+      end;
+
+      TTokenKind.CloseParen: begin
+        dec(paren_pairs);
+      end;
+    end;
+    get_next_token;
+  end;
+  if paren_pairs <> 0 then
+    self.append_error('Paren-pairs balance expected.', self.curr_token.pos);
 end;
 
 procedure TTester.test_operand();
+var prev_token : TToken;
+var forw_token : TToken;
 begin
-  if not self.inited then
-    raise Exception.Create('Call init procedure first!');
+  //check right
+  if self.curr_token_num > 0 then begin
+    prev_token := self.tokens[self.curr_token_num-1];
+    case prev_token.kind of
+      TTokenKind.CloseParen, TTokenKind.Constant, TTokenKind.Variable:
+        self.append_error_formatted('Operator before operand',prev_token.data);
+    end;
+  end;
+  //check left
+  if self.curr_token_num < length(self.tokens)-1 then begin
+    forw_token := self.tokens[self.curr_token_num+1];
+    case forw_token.kind of
+      TTokenKind.OpenParen, TTokenKind.Constant, TTokenKind.Variable:
+        self.append_error_formatted('Operator after operand',forw_token.data);
+    end;
+  end;
+end;
 
-  if self.curr_token.kind = TTokenKind.Op then begin
-    if self.curr_token.data = '(' then begin
-        self.get_next_token;
-        self.test_expr;
-        if self.curr_token.data <> ')' then
-          self.append_error('Close paren expected, but met ' + self.curr_token.data);
-    end
+procedure TTester.test_operator();
+var prev_token : TToken;
+var forw_token : TToken;
+begin
+  //check right
+  if self.curr_token_num > 0 then begin
+    prev_token := self.tokens[self.curr_token_num-1];
+    case prev_token.kind of
+      TTokenKind.Op, TTokenKind.OpenParen:
+        self.append_error_formatted('Operand before operator',prev_token.data);
+    end;
   end
   else
-    self.get_next_token;
+    self.append_error_formatted('Operand before operator',self.curr_token.data);
+
+  //check left
+  if self.curr_token_num < length(self.tokens)-1 then begin
+    forw_token := self.tokens[self.curr_token_num+1];
+    case forw_token.kind of
+      TTokenKind.Op, TTokenKind.CloseParen:
+        self.append_error_formatted('Operator after operand',forw_token.data);
+    end;
+  end
+  else
+    self.append_error_formatted('Operand after operator',self.curr_token.data);
 end;
 
 procedure TTester.get_next_token();
+var eol_token : ^TToken;
 begin
   if not self.inited then
     raise Exception.Create('Call init procedure first!');
-  if self.curr_token_num + 1 >= length(self.tokens) then
-    exit
+  if self.curr_token_num + 1 > length(self.tokens)-1 then begin
+  //
+    new(eol_token);
+    eol_token.data := 'EOL';
+    eol_token.kind := TTokenKind.EOL;
+    eol_token.pos.pos_in := self.curr_token.pos.pos_in + length(self.curr_token.data);
+    self.curr_token := eol_token^;
+    dispose(eol_token);
+  //
+  end
   else begin
     inc(self.curr_token_num);
     self.curr_token := self.tokens[self.curr_token_num];
   end;
 end;
 
-procedure TTester.append_error(msg : string);
+procedure TTester.append_error(msg : string; pos : TPosition);
 var error : ^TError;
 begin
   if not self.inited then
     raise Exception.Create('Call init procedure first!');
+  //
   New(error);
   error.msg  := msg;
+  error.pos  := pos;
   error.kind := TErrorKind.CONVERTER_ERROR;
-  error.pos  := self.curr_token.pos;
-
+  //
   SetLength(self.errors, length(self.errors)+1);
   self.errors[length(self.errors)-1] := error^;
 end;
 
+procedure TTester.append_error_formatted(expected : string; met : string);
+begin
+  self.append_error(expected + ' expected, but met: [' + met + ']', self.curr_token.pos);
+end;
+
+function TTester.match(kind : TTokenKind; offset : int = 0) : bool;
+begin
+  if length(self.tokens) > self.curr_token_num + offset then
+    if self.tokens[self.curr_token_num + offset].kind = kind then
+      exit(true);
+  exit(false);
+end;
+
 end.
+
