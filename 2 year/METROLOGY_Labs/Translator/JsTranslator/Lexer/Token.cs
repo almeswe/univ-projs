3645ea216d7ae09d473da5d0ffc0e1800ba3dfdd
iammelnikita <iammelnikita.work@yandex.by>
2021-10-15 22:39:37 +0300

    Update Token.cs

--- 2 year/METROLOGY_Labs/Translator/JsTranslator/Lexer/Token.cs
+++ 2 year/METROLOGY_Labs/Translator/JsTranslator/Lexer/Token.cs
@@ -8,7 +8,98 @@ namespace Translator.JsTranslator.Lexer
 {
     public enum TokenKind
     {
-        //...
+        TokenKeywordAwait, //Keywords
+        TokenKeywordBreak,
+        TokenKeywordCase,
+        TokenKeywordCatch,
+        TokenKeywordClass,
+        TokenKeywordConst,
+        TokenKeywordContinue,
+        TokenKeywordDebugger,
+        TokenKeywordDefault,
+        TokenKeywordDelete,
+        TokenKeywordDo,
+        TokenKeywordElse,
+        TokenKeywordEnum,
+        TokenKeywordExport,
+        TokenKeywordExtends,
+        TokenKeywordFalse,
+        TokenKeywordFinally,
+        TokenKeywordFor,
+        TokenKeywordFunction,
+        TokenKeywordIf,
+        TokenKeywordImplements,
+        TokenKeywordImport,
+        TokenKeywordIn,
+        TokenKeywordInstanceof,
+        TokenKeywordInterface,
+        TokenKeywordLet,
+        TokenKeywordNew,
+        TokenKeywordNull,
+        TokenKeywordPackage,
+        TokenKeywordPrivate,
+        TokenKeywordProtected,
+        TokenKeywordPublick,
+        TokenKeywordReturn,
+        TokenKeywordSuper,
+        TokenKeywordSwitch,
+        TokenKeywordStatic,
+        TokenKeywordThis,
+        TokenKeywordThrow,
+        TokenKeywordTry,
+        TokenKeywordTrue,
+        TokenKeywordTypeof,
+        TokenKeywordVar,
+        TokenKeywordVoid,
+        TokenKeywordWhile,
+        TokenKeywordWith,
+        TokenKeywordYield,
+
+        TokenOpParen, //Keychars
+        TokenClParen,
+        TokenOpBracket,
+        TokenClBracket,
+        TokenOpBrace,
+        TokenClBrace,
+        TokenQuestion,
+        TokenColon,
+        TokenSemicolon,
+        TokenDot,
+        TokenSQuote,
+        TokenDQuote,
+
+        TokenPlus, // ARITH
+        TokenDash,
+        TokenAsterisk,
+        TokenSlash,
+        TokenModulus,
+        TokenInc,
+        TokenDec,
+
+        TokenLgEq, // LOGICAL
+        TokenLgTypeEq, 
+        TokenLgNeq,
+        Token, // "<"
+        Token, // ">"
+        TokenGreaterEqThan,
+        TokenLessEqThan,
+        TokenLgEnd,
+        TokenLgOr,
+        TokenLgNot, 
+
+        TokenAssign, //ASSIGN
+        TokenAddAssign,
+        TokenSubAssign,
+        TokenMulAssign,
+        TokenDivAssign,
+        TokenModAssign,
+        TokenAmpersand,
+        TokenBar,
+        TokenCaret,
+        TokenTilde,
+        TokenLShift,
+        TokenRShift,
+        Token, // ">>>"
     }
 
     public sealed class Token
