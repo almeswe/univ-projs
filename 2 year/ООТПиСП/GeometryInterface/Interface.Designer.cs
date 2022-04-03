be59c756ce75e6ff323b7ab8e19d1cb6ab5b10aa
alexey <52577119+almeswe@users.noreply.github.com>
2022-04-04 00:20:28 +0300

    2nd year ООП lab2 updated

--- 2 year/ООТПиСП/GeometryInterface/Interface.Designer.cs
+++ 2 year/ООТПиСП/GeometryInterface/Interface.Designer.cs
@@ -29,6 +29,7 @@ namespace GeometryInterface
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Interface));
             this.ScriptTextBox = new System.Windows.Forms.TextBox();
             this.EvaluateButton = new System.Windows.Forms.Button();
             this.PictureBox = new System.Windows.Forms.PictureBox();
@@ -41,23 +42,25 @@ namespace GeometryInterface
             // 
             // ScriptTextBox
             // 
-            this.ScriptTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.ScriptTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.ScriptTextBox.Font = new System.Drawing.Font("Consolas", 12F);
             this.ScriptTextBox.Location = new System.Drawing.Point(4, 3);
             this.ScriptTextBox.Multiline = true;
             this.ScriptTextBox.Name = "ScriptTextBox";
             this.ScriptTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.ScriptTextBox.Size = new System.Drawing.Size(272, 288);
+            this.ScriptTextBox.Size = new System.Drawing.Size(435, 593);
             this.ScriptTextBox.TabIndex = 0;
+            this.ScriptTextBox.Text = resources.GetString("ScriptTextBox.Text");
             // 
             // EvaluateButton
             // 
-            this.EvaluateButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.EvaluateButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.EvaluateButton.Location = new System.Drawing.Point(3, 292);
+            this.EvaluateButton.Location = new System.Drawing.Point(3, 602);
             this.EvaluateButton.Name = "EvaluateButton";
-            this.EvaluateButton.Size = new System.Drawing.Size(273, 32);
+            this.EvaluateButton.Size = new System.Drawing.Size(436, 32);
             this.EvaluateButton.TabIndex = 2;
             this.EvaluateButton.Text = "EVALUATE";
             this.EvaluateButton.UseVisualStyleBackColor = true;
@@ -70,18 +73,18 @@ namespace GeometryInterface
             | System.Windows.Forms.AnchorStyles.Right)));
             this.PictureBox.Location = new System.Drawing.Point(0, 0);
             this.PictureBox.Name = "PictureBox";
-            this.PictureBox.Size = new System.Drawing.Size(965, 555);
+            this.PictureBox.Size = new System.Drawing.Size(965, 664);
             this.PictureBox.TabIndex = 3;
             this.PictureBox.TabStop = false;
             this.PictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBoxPaint);
             // 
             // LoadScriptButton
             // 
-            this.LoadScriptButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.LoadScriptButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.LoadScriptButton.Location = new System.Drawing.Point(3, 326);
+            this.LoadScriptButton.Location = new System.Drawing.Point(3, 636);
             this.LoadScriptButton.Name = "LoadScriptButton";
-            this.LoadScriptButton.Size = new System.Drawing.Size(273, 32);
+            this.LoadScriptButton.Size = new System.Drawing.Size(436, 32);
             this.LoadScriptButton.TabIndex = 4;
             this.LoadScriptButton.Text = "LOAD SCRIPT";
             this.LoadScriptButton.UseVisualStyleBackColor = true;
@@ -97,16 +100,16 @@ namespace GeometryInterface
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.Panel.Controls.Add(this.PictureBox);
-            this.Panel.Location = new System.Drawing.Point(279, 3);
+            this.Panel.Location = new System.Drawing.Point(442, 3);
             this.Panel.Name = "Panel";
-            this.Panel.Size = new System.Drawing.Size(967, 557);
+            this.Panel.Size = new System.Drawing.Size(967, 666);
             this.Panel.TabIndex = 5;
             // 
             // Interface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1248, 563);
+            this.ClientSize = new System.Drawing.Size(1411, 672);
             this.Controls.Add(this.Panel);
             this.Controls.Add(this.LoadScriptButton);
             this.Controls.Add(this.EvaluateButton);
