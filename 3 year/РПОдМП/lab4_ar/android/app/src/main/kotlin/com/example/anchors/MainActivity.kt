a31d0151e360c4cd2e3c530a7c656548b62d712a
alexey <52577119+almeswe@users.noreply.github.com>
2023-05-04 15:16:41 +0300

    3rd year РПОдМП Lab4 added

---
+++ 3 year/РПОдМП/lab4_ar/android/app/src/main/kotlin/com/example/anchors/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.anchors
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 3 year/РПОдМП/lab4_ar/lib/pages/home.dart
@@ -0,0 +1,122 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:ar_flutter_plugin/managers/ar_location_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
+import 'package:ar_flutter_plugin/models/ar_anchor.dart';
+import 'package:flutter/material.dart';
+import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
+import 'package:ar_flutter_plugin/datatypes/config_planedetection.dart';
+import 'package:ar_flutter_plugin/datatypes/node_types.dart';
+import 'package:ar_flutter_plugin/datatypes/hittest_result_types.dart';
+import 'package:ar_flutter_plugin/models/ar_node.dart';
+import 'package:ar_flutter_plugin/models/ar_hittest_result.dart';
+import 'package:vector_math/vector_math_64.dart';
+
+class ObjectGesturesWidget extends StatefulWidget {
+  const ObjectGesturesWidget({Key? key}) : super(key: key);
+  @override
+  _ObjectGesturesWidgetState createState() => _ObjectGesturesWidgetState();
+}
+
+class _ObjectGesturesWidgetState extends State<ObjectGesturesWidget> {
+  ARSessionManager? arSessionManager;
+  ARObjectManager? arObjectManager;
+  ARAnchorManager? arAnchorManager;
+
+  List<ARNode> nodes = [];
+  List<ARAnchor> anchors = [];
+
+  @override
+  void dispose() {
+    super.dispose();
+    arSessionManager!.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Object Transformation Gestures'),
+        ),
+        body: Stack(children: [
+          ARView(
+        onARViewCreated: onARViewCreated,
+        planeDetectionConfig: PlaneDetectionConfig.horizontalAndVertical,
+          ),
+          Align(
+        alignment: FractionalOffset.bottomCenter,
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                  onPressed: onRemoveEverything,
+                  child: const Text("Remove Everything")),
+            ]),
+          )
+        ]));
+  }
+
+  void onARViewCreated(
+      ARSessionManager arSessionManager,
+      ARObjectManager arObjectManager,
+      ARAnchorManager arAnchorManager,
+      ARLocationManager arLocationManager) {
+    this.arSessionManager = arSessionManager;
+    this.arObjectManager = arObjectManager;
+    this.arAnchorManager = arAnchorManager;
+
+    this.arSessionManager!.onInitialize(
+          showFeaturePoints: false,
+          showPlanes: true,
+          showWorldOrigin: true,
+          handlePans: true,
+          handleRotation: true,
+        );
+    this.arObjectManager!.onInitialize();
+
+    this.arSessionManager!.onPlaneOrPointTap = onPlaneOrPointTapped;
+  }
+
+  Future<void> onRemoveEverything() async {
+    /*nodes.forEach((node) {
+      this.arObjectManager.removeNode(node);
+    });*/
+    for (var anchor in anchors) {
+      arAnchorManager!.removeAnchor(anchor);
+    }
+    anchors = [];
+  }
+
+  Future<void> onPlaneOrPointTapped(
+      List<ARHitTestResult> hitTestResults) async {
+    var singleHitTestResult = hitTestResults.firstWhere(
+        (hitTestResult) => hitTestResult.type == ARHitTestResultType.plane);
+    if (singleHitTestResult != null) {
+      var newAnchor =
+          ARPlaneAnchor(transformation: singleHitTestResult.worldTransform);
+      bool? didAddAnchor = await arAnchorManager!.addAnchor(newAnchor);
+      if (didAddAnchor!) {
+        anchors.add(newAnchor);
+        // Add note to anchor3
+        var newNode = ARNode(
+            type: NodeType.webGLB,
+            uri:
+                "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Fox/glTF-Binary/Fox.glb",
+            scale: Vector3(0.2, 0.2, 0.2),
+            position: Vector3(0.0, 0.0, 0.0),
+            rotation: Vector4(1.0, 0.0, 0.0, 0.0));
+        bool? didAddNodeToAnchor =
+            await arObjectManager!.addNode(newNode, planeAnchor: newAnchor);
+        if (didAddNodeToAnchor!) {
+          nodes.add(newNode);
+        } else {
+          arSessionManager!.onError("Adding Node to Anchor failed");
+        }
+      } else {
+        arSessionManager!.onError("Adding Anchor failed");
+      }
+    }
+  }
+}
