76cb85161457f7aaa35e25931db7743df95e4190
alexey <52577119+almeswe@users.noreply.github.com>
2023-11-08 23:00:48 +0300

    4th year ПОВС Lab3 (c# part) added

---
+++ 4 year/АКГ/cga/Interaction.Com/Program.cs
@@ -0,0 +1,75 @@
+﻿using Visual;
+using System.Text;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace Interaction.Com
+{
+	public class Program
+	{
+		private static SceneForm _scene = null;
+
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			_scene = new SceneForm(@"C:\Users\HP\Desktop\car.obj");
+			if (ThreadPool.QueueUserWorkItem((o) => StartComCommunication()))
+				_scene.ShowDialog();
+		}
+
+		private static void StartComCommunication()
+		{
+			while (true)
+			{
+				var portNames = SerialPort.GetPortNames();
+				if (portNames.Length == 0)
+				{
+					Console.WriteLine("There are no available COM ports!\nTimeout...");
+					Thread.Sleep(5000);
+					continue;
+				}
+				Console.WriteLine($"Select COM port:");
+				foreach (var portName in portNames)
+					Console.WriteLine($"-- {portName}");
+				var portNameStr = Console.ReadLine();
+				try
+				{
+					var port = new SerialPort(portNameStr);
+					port.NewLine = "\r\n";
+					port.BaudRate = 115200;
+					port.Encoding = Encoding.ASCII;
+					port.Open();
+					var value = int.MaxValue;
+					while (port.IsOpen)
+						ComProcessReceivedData(port, ref value);
+				}
+				catch (Exception e) 
+				{
+					Console.WriteLine(e.Message);	
+				}
+			}
+		}
+
+		private static void ComProcessReceivedData(SerialPort port, ref int value)
+		{
+			var scale = 10;
+			var outValue = 0;
+			var data = port.ReadLine();
+			if (!data.Contains("CHCK"))
+				return;
+			var valueStr = data.Split("CHCK")[1].Trim();
+			Console.WriteLine($"{data} -> {valueStr}");
+			if (!int.TryParse(valueStr, out outValue))
+				return;
+			if (value != int.MaxValue)
+			{
+				var diff = value - outValue;
+				var events = int.Abs(diff / scale);
+				for (int i = 0; i < events; i++)
+					_scene.OnSceneKeyDown(null, new KeyEventArgs(
+						diff < 0 ? Keys.Left : Keys.Right));
+			}
+			value = outValue;
+		}
+	}
+}
