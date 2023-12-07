9e57c8b545acf3e5d5668ce1d5d6c3947211c5f1
almeswe <armag2003@mail.ru>
2023-12-07 16:55:18 +0300

    4th year ЦОС Lab4 added

--- 4 year/ЦОС/Lab4/main.py
+++ 4 year/ЦОС/Lab4/main.py
@@ -4,6 +4,8 @@ from PIL import Image
 from PIL import ImageOps
 from tqdm import tqdm
 
+from scipy.ndimage import shift
+
 class ImageProcessing(object):
     def __init__(self, im1: Image, im2: Image = None) -> None:
         self.im1: Image = im1
@@ -37,11 +39,13 @@ class ImageProcessing(object):
         return corr_npa
 
     def autocorr(self) -> np.ndarray:
-        from scipy import fftpack
+        height, width = self.im1_npa.shape
+        autocorr = np.zeros(shape=(height, width))
         n_im1_npa: np.ndarray = self.norm(self.im1_npa)
-        f: np.ndarray = fftpack.fftn(n_im1_npa)
-        fps: np.ndarray = np.abs(f) ** 2
-        return np.array(fftpack.ifftn(fps).real)
+        for dy in tqdm(range(height), leave=False):
+            for dx in tqdm(range(width), leave=False):
+                autocorr[dy, dx] = np.sum(n_im1_npa * np.roll(n_im1_npa, shift=(dy, dx), axis=(0, 1)))
+        return autocorr
 
     def show_corr(self, corr: np.ndarray) -> None:
         import matplotlib.pyplot as plt
@@ -60,10 +64,41 @@ class ImageProcessing(object):
 
     def show_autocorr(self, autocorr: np.ndarray) -> None:
         import matplotlib.pyplot as plt
+        plt.imshow(self.im1, cmap='gray')
+        plt.show()
+    
         plt.imshow(autocorr, cmap='hot')
         plt.colorbar(label='autocorr')
         plt.show()
 
+    '''
+    @staticmethod
+    def fft(x: np.ndarray) -> np.ndarray:
+        n: int = len(x)
+        if n <= 1: 
+            return x
+        e: np.ndarray = ImageProcessing.fft(x[0::2])
+        o: np.ndarray = ImageProcessing.fft(x[1::2])
+        temp: np.ndarray = [np.exp(-2j*np.pi*k/n)*o[k] for k in range(n//2)]
+        return np.array([e[k] + temp[k] for k in range(n//2)] + [e[k] - temp[k] for k in range(n//2)])
+
+    @staticmethod
+    def ifft(x: np.ndarray) -> np.ndarray:
+        n: int = len(x)
+        if n <= 1:
+            return x
+        e: np.ndarray = ImageProcessing.ifft(x[0::2])
+        o: np.ndarray = ImageProcessing.ifft(x[1::2])
+        temp: np.ndarray = [np.exp(2j*np.pi*k/n)*o[k] for k in range(n//2)]
+        return np.array([e[k] + temp[k] for k in range(n//2)] + [e[k] - temp[k] for k in range(n//2)]) / 2
+
+    @staticmethod
+    def fft2d(x: np.ndarray) -> np.ndarray:
+        fft_rows = np.array([ImageProcessing.fft(row) for row in x])
+        fft_2d = np.array([ImageProcessing.fft(column) for column in fft_rows.T]).T
+        return fft_2d
+    '''
+        
 if __name__ == '__main__':
     import sys
     if len(sys.argv) == 2:
@@ -80,9 +115,9 @@ if __name__ == '__main__':
         )
         corr = imp.corr()
         imp.show_corr(corr)
-    '''
+    exit(0)
     ImageProcessing(
         Image.open(sys.argv[1]),
         Image.open(sys.argv[2]),
     )
-    '''
+    #ImageProcessing(Image.open("./test_image.jpg")).autocorr()
